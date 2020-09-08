3e97455d4a24bc2c625f98f84928275707725650
Philip Lind Jensen <jogjensen@hotmail.com>
2020-09-08 10:55:03 +0200

    Add project files.

---
+++ Bike/Bikes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Bike
+{
+    public class Bikes
+    {
+        private int _id;
+        private string _color;
+        private int _price;
+        private int _gear;
+
+        public Bikes(int id, string color, int price, int gear)
+        {
+            _id = id;
+            _color = color;
+            _price = price;
+            _gear = gear;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Color
+        {
+            get => _color;
+            set => _color = value;
+        }
+
+        public int Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+
+        public int Gear
+        {
+            get => _gear;
+            set => _gear = value;
+        }
+    }
+}
